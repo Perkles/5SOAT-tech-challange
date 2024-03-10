ba7c0a0c5a50da2c0455faabb447aa6b22d9e79b
Perkles <otaviocha@gmail.com>
2024-03-10 17:40:45 -0300

    refactor: modifica nome de arquivo ClienteUseCase.ts para Cliente.usecase.ts
    Modificação para manter a convensao de nomenclatura dos arquivos

--- src/core/domain/useCases/Cliente.usecase.ts
+++ src/core/domain/useCases/Cliente.usecase.ts
@@ -1,1 +1,31 @@
+import { UseCaseException } from "../../../adapters/exceptions/UseCase.exception";
+import { ClienteAdapterGateway } from "../../../adapters/gateways/Cliente.gateway";
+import { Cliente } from "../entities/Cliente";
+import { Cpf } from "../valueObjects/Cpf.vo";
+import { Email } from "../valueObjects/Email.vo";
 
+
+export class ClienteUseCase {
+    static async cadastraClientePorCpf(cpf: string, clienteGateway: ClienteAdapterGateway): Promise<Cliente> {
+        const clienteExistente = await ClienteUseCase.buscaClientePorCpf(cpf, clienteGateway);
+        if(clienteExistente){
+            throw new UseCaseException(`Um usuário com CPF: ${cpf} ja existe `)
+        }
+        const cliente = new Cliente({
+            cpf: new Cpf(cpf)
+        });
+        return await clienteGateway.salvaCliente(cliente);
+    }
+
+    static async cadastraClientePorNomeEmail(nome: string, email: string, clienteGateway: ClienteAdapterGateway): Promise<Cliente> {
+        const cliente = new Cliente({
+            nome: nome,
+            email: new Email(email)
+        });
+        return await clienteGateway.salvaCliente(cliente);
+    }
+
+    static async buscaClientePorCpf(cpf: string, clienteGateway: ClienteAdapterGateway): Promise<Cliente | undefined> {
+        return await clienteGateway.buscaPorCpf(new Cpf(cpf));
+    }
+}
